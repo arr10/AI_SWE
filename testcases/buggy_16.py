b082bdc23cab9b5c08bc987b3c0dd2a3da851981
sultan sultanidinov <sultanidinov0567@gmail.com>
2023-12-06 19:08:45 +0900

    finished crossover, and some part of the dataset

---
+++ testcases/buggy_16.py
@@ -0,0 +1,5 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
---
+++ testcases/test_cases_17.py
@@ -0,0 +1,8 @@
+def test1():
+    assert factorial(1) == 1
+
+def test2():
+    assert factorial(15) == 1307674368000  
+
+def test3():
+    assert factorial(132) == 8683317618811886495518194401280000000
